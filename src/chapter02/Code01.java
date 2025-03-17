9eb0403af1c1f855936835ed8e2ac9d3d859e99b
hyunhyesoo <sue990831@gmail.com>
2025-03-17 12:06:27 +0900

    Main Method 사용법 연결연산자, 덧셈연산자 차이점 println,print 차이점

--- src/chapter02/Code01.java
+++ src/chapter02/Code01.java
@@ -8,4 +8,5 @@ public class Code01 {
         System.out.println("100 + 200 = " + (100+200));//숫자 뒤에 있을 때는 먼저 계산하려면 괄호가 필요함
         System.out.println(100 + 200 + "won");//숫자가 앞에 있으면 괄호 없이도 먼저 계산됨
     }
+
 }
