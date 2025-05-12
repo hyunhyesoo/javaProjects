ac8b698ee360bbc15da41afc10771302415d8d0a
hyunhyesoo <sue990831@gmail.com>
2025-05-12 10:52:07 +0900

    6장 반복문-For6, 중첩 for문

---
+++ src/chapter06/ForTest6.java
@@ -0,0 +1,12 @@
+package chapter06;
+
+public class ForTest6 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.printf("인공지능소프트웨어과 알고리즘 실습: (i: %d, j: %d) \n", i, j);
+            }
+        }
+    }
+}
