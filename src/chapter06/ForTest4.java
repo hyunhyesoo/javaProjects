31b6c25b32e0f9a35130ca0a90894dadd3aaf901
hyunhyesoo <sue990831@gmail.com>
2025-05-12 10:34:47 +0900

    6장 반복문-For4, 홀수의 합계를 구하는 프로그램으로 수정

--- src/chapter06/ForTest4.java
+++ src/chapter06/ForTest4.java
@@ -4,9 +4,13 @@ public class ForTest4 {
     public static void main(String[] args) {
         int n = 1000;
         int hap = 0;
+        String oddeven = "";
         for (int i = 1; i <= n; i++) {
+            if(i % 2 != 0)
             hap += i;
+            oddeven = "홀수";
         }
-        System.out.println("1부터 " + n + "까지의 합: " + hap);
+        System.out.println("1부터 " + n + "까지의 " + oddeven + " 합: " + hap);
+//        System.out.println("1부터 " + n + "까지의 합: " + hap);
     }
 }
